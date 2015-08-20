e7015e5e96777a2126ff969e0187e48f21219524
Anthony De Meulemeester <anthonydm@bubblepost.be>
2015-08-20 12:02:41 +0200

    Update main.go

--- main.go
+++ main.go
@@ -18,5 +18,5 @@ func main() {
 }
 
 func helloHandler(ctx *weavebox.Context) error {
-	return ctx.Text(http.StatusOK, "Hello you from a weavebox app in a fancy docker container")
+	return ctx.Text(http.StatusOK, "Hello you")
 }
